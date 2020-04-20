8a688a602ecdaebf487440e27869c1793a7b7779
joshuaasare <joshuaasare29@gmail.com>
2020-04-20 13:38:34 +0000

    lock file

--- src/_shared/constants.js
+++ src/_shared/constants.js
@@ -7,13 +7,7 @@ export const constants = {
     USER_LOGIN_ATTEMPTS: 'user_login_attempts',
     NUMBER_OF_ATTEMPTS: '3',
   },
-  api: {
-    //  ROOT_URL: 'http://192.168.204.2:3000', // emulatir
-    //  ROOT_URL: 'http://192.168.0.101:3000', // the mifi
-    //  ROOT_URL: 'http://10.74.81.74:3000',
-    ROOT_URL: 'http://192.168.0.100:3000',
-    // ROOT_URL: 'http://100.101.7.149:3000', // the cloud
-  },
+  api: {},
   subscribableEvents: {
     SHOPS_UPDATED: 'shops_updated',
   },
@@ -23,18 +17,17 @@ export const constants = {
     PAID_FOR_TODAY: 'Paid',
     NOT_PAID_FOR_TODAY: 'Not Paid',
   },
-  map: {
-    GOOGLE_MAPS_API_KEY: 'AIzaSyAH9gmtyYEOZYi6WJP6auIEX805eb9BNwQ',
-    GOOGLE_DIRECTIONS_BASE_URL: 'https://maps.googleapis.com/maps/api/directions/json',
-  },
+  map: {},
   errorMessages: {
     LOCATION_PERMISSION_NOT_GRANTED:
       'You need to grant location permissions to Market toll to use this feature',
-    POSITION_UNAVAILABLE: 'We could not determine your position. Please try again!',
+    POSITION_UNAVAILABLE:
+      'We could not determine your position. Please try again!',
     REQUEST_TIMEOUT: 'Please check internet connection and try again',
     PLAY_SERVICES_UNAVAILABLE:
       'Google play services is not installed or out of date, please install and try again!',
-    LOCATION_SERVICE_UNAVAILABLE: 'Please turn on location services and try again',
+    LOCATION_SERVICE_UNAVAILABLE:
+      'Please turn on location services and try again',
     INTERNAL_CRASH: 'Oops, there seems to be a problem, Please try again',
     NO_LOCATION: 'Oops, the location for this shop has not been stored',
     CAMERA_SERVICE_UNAVAILABLE: 'Please turn on camera service to try again',
@@ -53,3 +46,11 @@ export const constants = {
     EXTRA_LARGE_SCREEN_WIDTH: 1024,
   },
 };
+
+
+const personalize_msg = (Name, _ending = '.') => {
+  const message = 'you are great';
+  return `${Name}, ${message}${_ending}`;
+};
+
+personalize_msg('Anonymous', '!');
